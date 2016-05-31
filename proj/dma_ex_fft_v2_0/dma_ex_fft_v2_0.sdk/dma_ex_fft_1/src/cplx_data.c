ef8f97dd36592a2b14bd3328fd676ebd9d469e1e
anderash <anderash@stud.ntnu.no>
2016-05-31 15:00:06 +0200

    Added all project files created by Vivado and Xilinx SDK

---
+++ proj/dma_ex_fft_v2_0/dma_ex_fft_v2_0.sdk/dma_ex_fft_1/src/cplx_data.c
@@ -0,0 +1,10 @@
+
+// Includes
+#include <stdio.h>
+#include "cplx_data.h"
+
+// Public functions
+void cplx_data_get_string(char* c, cplx_data_t data)
+{
+	sprintf(c, "%d + j*%d", data.data_re, data.data_im);
+}
